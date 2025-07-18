8c926771bc9bbf3f0a31fd554696fc766c6251ca
TeamRthree <team@rthree.in>
2025-07-18 16:44:23 +0530

    Test auto deploy: added /test-deploy route

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\EnquiryController;
 
 Route::post('/enquiry', [EnquiryController::class, 'store']);
 
+Route::get('/test-deploy', function () {
+    return response()->json(['message' => 'Deployment works!']);
+});
